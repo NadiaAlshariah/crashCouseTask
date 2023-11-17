073fc723acc0d7b3ecc15365728495a0139e7575
NadiaAlshariah <nadiakalshariah@gmail.com>
2023-11-17 17:52:49 +0300

    spring initilzed

---
+++ src/main/java/com/example/crashCourseTask/CrashCourseTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.crashCourseTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrashCourseTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrashCourseTaskApplication.class, args);
+	}
+
+}
